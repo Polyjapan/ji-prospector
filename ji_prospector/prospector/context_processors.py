1ce8ab806870634e00eb63d51ddfbce0bf66f4e6
Arnaud Gaudard <gaudard@protonmail.ch>
2020-05-02 16:23:20 +0200

    stupid mistake

--- ji_prospector/prospector/context_processors.py
+++ ji_prospector/prospector/context_processors.py
@@ -1,8 +1,11 @@
+from django.db import transaction
+
 from .models import Event
 
 def current_event(request):
     try:
-        e = Event.objects.select_for_update().get(current=True)
+        with transaction.atomic():
+            e = Event.objects.select_for_update().get(current=True)
     except Event.DoesNotExist:
         e = None
     return {'current_event': e}
